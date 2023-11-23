c97a83e4d7296db538e25c70b8007eeecc897376
FloKit <f.kittel@gmx.net>
2023-11-23 09:59:45 +0100

    initial commit

---
+++ native-app/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Button style={styles.button} title="I am hungry!"></Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: { height: 30, backgroundColor: "blue", width: 100 },
+});
