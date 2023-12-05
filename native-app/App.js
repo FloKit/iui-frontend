1c0243cb3129885d89400ad04d74bdab43322228
FloKit <f.kittel@gmx.net>
2023-12-05 20:31:10 +0100

    use navigation

--- native-app/App.js
+++ native-app/App.js
@@ -1,14 +1,54 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
-import ResultList from "./components/ResultList";
-import Header from "./components/Header";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import RestaurantListScreen from "./screens/RestaurantListScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
+
+const AppTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "#fff",
+  },
+};
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const [showList, setShowList] = useState(false);
   return (
-    <SafeAreaView>
-      <Header />
-      <ResultList />
-    </SafeAreaView>
+    <NavigationContainer theme={AppTheme}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          options={{
+            headerShown: false,
+          }}
+        >
+          {(props) => (
+            <HomeScreen {...props} loading={loading} setLoading={setLoading} />
+          )}
+        </Stack.Screen>
+
+        <Stack.Screen
+          name="RestaurantList"
+          component={RestaurantListScreen}
+          options={{
+            title: "Restaurants Close By",
+            headerStyle: {
+              backgroundColor: "#F9A826",
+            },
+            headerTintColor: "#fff",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontFamily: "Inter",
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ native-app/screens/RestaurantListScreen/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import ResultList from "../../components/ResultList";
+
+export default function RestaurantListScreen() {
+  return (
+    <SafeAreaView>
+      <ResultList />
+    </SafeAreaView>
+  );
+}
